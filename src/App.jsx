b4ab0d602e8a85f85ef86ecddef6b764c0116a25
anshi08 <sabharwalanshika08@gmail.com>
2023-11-07 16:21:02 -0800

    Added:UseState with Prev State

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Js
+  // let counter = 0
+
+  // const increaseValue = () =>{
+  //   console.log("before",counter)
+  //    counter = counter + 1
+  //   console.log("after",counter)
+  // }
+
+  //useState
+  
+  let initialValue = 0;
+  let [counter,setCounter] = useState(initialValue)
+
+  const increaseValue = () =>{
+     setCounter(counter=>counter+1)
+  }
+
+  const decreaseValue = () =>{
+    setCounter(counter=>counter-1)
+ }
+
+ const resetValue = () => {
+    setCounter(initialValue)
+ }
+
+ //UseState With Previou State
+ const incrementFive = () =>{
+      setCounter(prevCount => prevCount + 5)
+ }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Counter {counter} </h2>
+      <button onClick={resetValue}>Reset Value</button><br/><br/>
+      <button onClick={increaseValue}>Increment</button><br/><br/>
+      <button onClick={decreaseValue}>Decrement</button><br/><br/>
+      <button onClick={incrementFive}>Increment by 5</button>
     </>
   )
 }
